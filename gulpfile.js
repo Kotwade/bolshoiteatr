6803983d6c2b489cc3c8d12ffb1ebb48e4ef31fc
Kotwade <kotwade@yandex.ru>
2021-06-09 12:45:24 +0500

    Сменил название

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,6 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
     "source/js/**",
     "source//*.ico"
     ], {
