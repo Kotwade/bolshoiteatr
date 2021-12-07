802112af9774a45e5d68699314117665fc89a05b
Kotwade <kotwade@yandex.ru>
2021-12-07 09:55:59 +0500

    Исправления 14

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,35 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var ESC_KEY = 'Escape';
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+var popup = document.querySelector('.jewellery-popup');
+
+if (popup) {
+  var popupOpen = document.querySelector('.portfolio__loup');
+  var popupClose = popup.querySelector('.jewellery-popup__button-cross');
+  var overlay = document.querySelector('.overlay');
+
+  var onCartPopupEscPress = function (evt) {
+    if (evt.key === ESC_KEY) {
+      closeCartPopup();
+    }
+  };
+
+  var openPopup = function () {
+    popup.classList.remove('jewellery-popup--hidden');
+    overlay.classList.remove('overlay--hidden');
+    document.addEventListener('keydown', onCartPopupEscPress);
+  };
+
+  var closeCartPopup = function () {
+    popup.classList.add('jewellery-popup--hidden');
+    overlay.classList.add('overlay--hidden');
+    document.removeEventListener('keydown', onCartPopupEscPress);
+  };
+
+  popupOpen.addEventListener('click', openPopup);
+
+  popupClose.addEventListener('click', closeCartPopup);
+
+  overlay.addEventListener('click', closeCartPopup);
+}
